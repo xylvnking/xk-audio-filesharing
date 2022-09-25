e3e6b67a2fe3419d4c60185e81f5a8a9f342284a
Dylan King <xylvnking@gmail.com>
2022-09-25 18:50:28 -0400

    created database reset api call, obviously to be removed from prod

--- pages/api/resetDatabase.js
+++ pages/api/resetDatabase.js
@@ -1,8 +1,91 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 // this call needs to reset the firebase database 
+import { db } from "../../firebase/firebase-config";
+import { collection, addDoc, doc, getDoc, setDoc, updateDoc, deleteDoc } from "firebase/firestore"; 
+import { fakeData } from "../../fakeData";
+
+export default async function handler(req, res) {
+
+
+
+
+    await deleteDoc(doc(db, 'projects', 'projectName1'))
+    await setDoc(doc(db, 'projects', 'projectName1'), { // create projects collection
+        metadata: {
+            projectName: 'projectName',
+            numberOfSongs: 3
+        },
+        songName1: 'songName1',
+        songName2: 'songName2',
+        songName3: 'songName3',
+
+    }).catch((error) => { console.log(error) })
+
+
+
+
+
+    await deleteDoc(doc(db, 'songs', 'songName1'))
+    await setDoc(doc(db, 'songs', 'songName1'), { // create songs collection
+        metadata: {
+            songField1: 'someValue1',
+            songField2: 'someValue2',
+        },
+        usersWithAccess: [ // array seems like the right choice
+            'someUID1',
+            'someUID2',
+            'someUID3',
+            'c6EqhwHBFCZ6qIPOQRfZp1UTFyo1',
+            'xylvnKing'
+        ]
+    }).catch((error) => { console.log(error) })
+    .then(
+        setDoc(doc(db, 'songs', 'songName1', 'fileVersions', 'fileVersionName1'), {
+            someFieldCreatedX: 'someValueCreatedX',
+            someFieldCreatedX2: 'someValueCreatedX2',
+        })
+    ).catch((error) => { console.log(error) })
+
+
+
+    await deleteDoc(doc(db, 'users', 'artistName1'))
+    await setDoc(doc(db, 'users', 'artistName1'), { // create users collection
+        metadata: {
+            artistName: 'artistName1',
+            someField: 'someValue',
+            email: 'fakeEmail@fakeEmail.com',
+            realName: 'someName',
+        },
+        songsAuthorizedOn: [
+            'songName1',
+            'songName2',
+            'songName3'
+        ]
+
+    }).catch((error) => { console.log(error) })
     
-export default function handler(req, res) {
-    res.status(200).json({ name: 'John Doe' })
-  }
-  
+    
+    
+    await deleteDoc(doc(db, 'users', 'xylvnKing'))
+    await setDoc(doc(db, 'users', 'xylvnKing'), { // create users collection
+        metadata: {
+            uid: 'c6EqhwHBFCZ6qIPOQRfZp1UTFyo1',
+            artistName: 'xylvnKing',
+            email: 'xylvnking@gmail.com',
+            realName: 'Dylan King',
+        },
+        songsAuthorizedOn: [
+            'songName1',
+            'songName2',
+            'songName3'
+        ]
+
+    }).catch((error) => { console.log(error) })
+
+
+
+
+
+    res.status(200).json({ name: 'it has been done' })
+}
