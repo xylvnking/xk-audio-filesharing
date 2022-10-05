aa508f68c4db572167293094232612a60f6432c5
Dylan King <xylvnking@gmail.com>
2022-10-05 16:42:13 -0400

    finished quick profile page

---
+++ components/Audio/ProfileFolder/ProfileMainComponent.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import styles from './Profile.module.scss'
+
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth, provider } from '../../../firebase/firebase-config'
+import { signInWithGoogle, signUserOut } from '../AuthFolder/Auth'
+
+export default function ProfileMainComponent() {
+    const [userAuth, userAuthIsLoading, userAuthError] = useAuthState(auth)
+
+    
+
+    // console.log(userAuth)
+    return (
+            // <h1>this is where the user will manage their info</h1>
+            <div>
+                {
+                    userAuth ?
+                    <main className={styles.container}>
+                        
+                        <ul>
+                            <li>
+                                <h2>
+                                    {userAuth.displayName}
+                                </h2>
+                            </li>
+                            <li>
+                                <h2>
+                                    {userAuth.email}
+                                </h2>
+                            </li>
+                            <li>
+                                <h2>
+                                    UID: <span style={{fontWeight: 400}}>{userAuth.uid}</span>
+                                </h2>
+                            </li>
+                            <li>
+                                <h2>
+                                    Account created: <span style={{fontWeight: 400}}>{userAuth.metadata.creationTime}</span>
+                                </h2>
+                            </li>
+                            <li>
+                                <h2>
+                                    Last sign in: <span style={{fontWeight: 400}}>{userAuth.metadata.lastSignInTime}</span>
+                                </h2>
+                            </li>
+                        </ul>
+                        <button onClick={signUserOut} className={styles.signOutButton}>Sign Out</button>
+                    </main>
+                    :
+                    <div className={styles.signInButtonContainer}>
+                        <button onClick={signInWithGoogle} className={styles.signInButton}>Sign In</button>
+                    </div>
+                }
+            </div>
+    )
+}
--- pages/audio/profile.js
+++ pages/audio/profile.js
@@ -3,18 +3,11 @@ import AudioLayout from '../../components/Audio/AudioLayout/AudioLayout'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { auth, provider } from '../../firebase/firebase-config'
 import { signInWithGoogle, signUserOut } from '../../components/Audio/AuthFolder/Auth'
+import ProfileMainComponent from '../../components/Audio/ProfileFolder/ProfileMainComponent'
 
 export default function Profile() {
-    const [userAuth, userAuthIsLoading, userAuthError] = useAuthState(auth)
     return (
-            // <h1>this is where the user will manage their info</h1>
-            <div>
-                {
-                    !userAuthIsLoading && userAuth && <button onClick={signUserOut}> Sign Out</button>
-                }
-                {
-                    !userAuthIsLoading && !userAuth && <button onClick={signInWithGoogle}> Sign In</button>
-                }
-            </div>
+
+        <ProfileMainComponent />
     )
 }
